8a6ce0bf15c52dace376ab6620fbd0f5e5b43209
juste-zd <71153585+juste-zd@users.noreply.github.com>
2026-09-18 23:42:39 +0300

    Update main.cpp
    Changed hw grade input to not ask for nr of grades, started on input validation, fixed bug in avg calc

--- main.cpp
+++ main.cpp
@@ -16,7 +16,6 @@ void stud_duom(studentas A, char metodas);
 
 int main()
 {
-    int k;
     std::vector<studentas> grupe;
     studentas A;
 
@@ -29,15 +28,30 @@ int main()
     for (int j = 0; j < n; j++) {
         std::cout<<"Iveskite studento varda ir pavarde per tarpa: ";
         std::cin>>A.vardas>>A.pavarde;
-        std::cout<<"Iveskite semestro pazymiu kieki: ";
-        std::cin>>k;
 
-        for (int i = 0; i < k; i++) {
-            std::cout<<"Iveskite "<< i+1 <<" pazymi: ";
-            int a;
-            std::cin>>a;
-            A.paz.push_back(a);
-        }
+        string ivestis; 
+        do {
+            std::cout<<"Iveskite pazymi (0-10) arba n, jei daugiau namu darbu rezultatu nera: ";
+            std::cin>>ivestis;
+            if (ivestis == "n") break;
+            bool skaicius = true;
+            for (char c: ivestis) {
+                if (c < '0' || c > '9') {
+                    skaicius = false;
+                    break;
+                } 
+            }
+            if (skaicius) {
+                int pazymys = std::stoi(ivestis);
+                if (pazymys >= 0 && pazymys <= 10) {
+                    A.paz.push_back(pazymys);
+                } 
+                else skaicius = false; 
+            } 
+            if (! skaicius) {
+                std::cout << "Neteisinga ivestis. Iveskite skaiciu nuo 0 iki 10 arba n.\n";
+            }
+        } while (true);
 
         std::cout<<"Iveskite egzamino pazymi: ";
         std::cin>>A.egz;
@@ -69,33 +83,34 @@ int main()
     }
     std::cout<<"\n";
     
-    for (studentas B : grupe)
+    for (studentas B: grupe)
         stud_duom(B, metodas);
 }
 
 //function to print students, final grades
-void stud_duom(studentas A, char metodas)
+void stud_duom(studentas C, char metodas)
 {
     float vidurkis = 0;
-    int kiekis = A.paz.size();
+    int kiekis = C.paz.size();
     if(metodas=='v') {
         float suma = 0;
-        for (int p: A.paz)
+        for (int p: C.paz)
             suma += p;
-        float vidurkis = suma / kiekis;
+        vidurkis = suma / kiekis;
     }
     else {
-        vector<int> v = A.paz;
+        vector<int> v = C.paz;
         std::sort(v.begin(), v.end());
         if (kiekis % 2 == 0) {
             vidurkis = (v[kiekis/2 - 1] + v[kiekis/2]) / 2.0;
         }
         else vidurkis = v[kiekis / 2];
     }
-    float galutinis = 0.4*vidurkis + 0.6*A.egz;
+    
+    float galutinis = 0.4*vidurkis + 0.6*C.egz;
 
     std::cout<<std::left
-        <<std::setw(15)<<A.vardas
-        <<std::setw(15)<<A.pavarde
+        <<std::setw(15)<<C.vardas
+        <<std::setw(15)<<C.pavarde
         <<std::setw(15)<<std::fixed<<std::setprecision(2)<<galutinis<<"\n";
 }
